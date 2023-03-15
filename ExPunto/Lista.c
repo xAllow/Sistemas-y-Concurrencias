3744b0324053013551373126da6937d5d7b7ff1e
xAllow <alvluqtoruma@uma.es>
2023-03-15 12:26:09 +0100

    expunto

---
+++ ExPunto/Lista.c
@@ -0,0 +1,124 @@
+#include "Lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/*
+ * Inicializa la lista de puntos creando una lista vacía
+ *
+ */
+void crearLista(TLista *lista){
+    *lista = NULL;
+}
+
+
+/**
+ * Inserta el punto de forma ordenada (por el valor de la abscisa x)
+ * en la lista siempre que no esté repetida la abscisa.
+ *  En ok, se devolverá un 1 si se ha podido insertar, y  0 en caso contrario.
+ *  Nota: utiliza una función auxiliar para saber
+ *   si ya hay un punto en la lista con la misma abscisa punto.x
+ *
+ */
+void insertarPunto(TLista *lista, struct Punto punto, int * ok){
+    if(buscarPunto(*lista, punto.x)){
+        *ok = 0;
+    } else {
+        TLista aux = malloc(sizeof(struct Nodo));
+        aux->punto.x = punto.x;
+        aux->punto.y = punto.y;
+        aux->sig = NULL;
+        TLista act = *lista;
+        TLista ant = NULL;
+        while(act != NULL && act->punto.x < punto.x){
+            ant = act;
+            act = act->sig;
+        }
+        if(ant == NULL){
+            aux->sig = act;
+            *lista = aux;
+        } else {
+            ant->sig = aux;
+            aux->sig = act;
+        }
+        *ok = 1;
+    }
+}
+
+int buscarPunto(TLista lista, float x) {
+    while (lista != NULL) {
+        if (lista->punto.x == x) {
+            return 1;
+        }
+        lista = lista->sig;
+    }
+    return 0;
+}
+/*
+ * Elimina de la lista el punto con abscisa x de la lista.
+ * En ok devolverá un 1 si se ha podido eliminar,
+ * y un 0 si no hay ningún punto en la lista con abscisa x
+ *
+ */
+void eliminarPunto(TLista *lista,float x,int* ok){
+    if(!buscarPunto(*lista, x)){
+        *ok = 0;
+    } else {
+        TLista act = *lista;
+        TLista ant = NULL;
+        while(act != NULL){
+            if(act->punto.x == x){
+                if(ant == NULL){
+                    *lista = act->sig;
+                    free(act);
+                } else {
+                    ant->sig = act->sig;
+                    free(act);
+                }
+                *ok = 1;
+            }
+            ant = act;
+            act = act->sig;
+        }
+    }
+}
+
+ /**
+ * Muestra en pantalla el listado de puntos
+ */
+void mostrarLista(TLista lista){
+    if(lista == NULL){
+        printf("Lista vacia\n");
+    } else {
+        TLista aux = lista;
+        while (aux != NULL){
+            printf("(x:%.2f, y:%.2f)\n", aux->punto.x,aux->punto.y);
+            aux = aux->sig;
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * Destruye la lista de puntos, liberando todos los nodos de la memoria.
+ */
+void destruir(TLista *lista){
+    TLista aux;
+    while (*lista != NULL){
+        aux = *lista;
+        *lista = (*lista)->sig;
+        free(aux);
+    }
+    *lista = NULL;
+}
+
+/*
+ * Lee el contenido del archivo binario de nombre nFichero,
+ * que contiene una secuencia de puntos de una función polinómica,
+ *  y lo inserta en la lista.
+ *
+ */
+void leePuntos(TLista *lista,char * nFichero){
+
+}
---
+++ ExPunto/Principal.c
@@ -0,0 +1,66 @@
+/*
+ * Principal.c
+ *
+ *  Created on: 16 jun. 2017
+ *      Author: mmar
+ */
+
+#include "Lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+float f(float x){
+	return x*x -625;
+}
+
+
+int main(){
+
+	srand(time(NULL));
+
+	int ok;
+	int i = 0;
+	TLista l;
+	crearLista(&l);
+
+    int v = 10;
+
+    printf("Número de puntos: %d\n",v);
+
+    struct Punto punto;
+    for (i=0;i<v;i++){
+    		punto.x = 100 * ((float)rand() / (float)RAND_MAX);
+    		punto.y = f(punto.x);
+    		printf("Introduce el punto (%.2f, %.2f)\n",punto.x,punto.y);
+    		insertarPunto(&l,punto,&ok);
+	}
+    punto.x = 25;
+    punto.y = f(punto.x);
+    printf("Introduce el punto (%.2f, %.2f)\n",punto.x,punto.y);
+    insertarPunto(&l,punto,&ok);
+	punto.x = 25;
+    punto.y = f(punto.x);
+	insertarPunto(&l,punto,&ok);
+    mostrarLista(l);
+	printf("Eliminar x = 25");
+    eliminarPunto(&l,25,&ok);
+    mostrarLista(l);
+    destruir(&l);
+    mostrarLista(l);
+	return 0;
+	
+}
+
+int main2(){
+	TLista l;
+	int ok;
+	leePuntos(&l,"Puntos.bin");
+	mostrarLista(l);
+	eliminarPunto(&l,25,&ok);
+	mostrarLista(l);
+	destruir(&l);
+	mostrarLista(l);
+	return 0;
+}
